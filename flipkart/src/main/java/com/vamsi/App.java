0eb437f547552eddabc7f780466ae3a8f0e3d311
root <root@learnings.asia-south1-b.c.silken-alloy-332902.internal>
2021-12-10 06:17:56 +0000

    Added flipkart code

---
+++ flipkart/src/main/java/com/vamsi/App.java
@@ -0,0 +1,13 @@
+package com.vamsi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
